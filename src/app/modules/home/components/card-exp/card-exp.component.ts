5f559cc5733b615d7f6bf215235a2d7e206c7673
Kleisson <kleissonsm.dev@gmail.com>
2025-10-28 12:08:44 -0300

    feat: criado card exp

---
+++ src/app/modules/home/components/card-exp/card-exp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-exp',
+  imports: [],
+  templateUrl: './card-exp.component.html',
+  standalone: true,
+  styleUrl: './card-exp.component.scss'
+})
+export class CardExpComponent {
+
+}
